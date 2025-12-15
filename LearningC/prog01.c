c41ef32348e8a85ca8eb1e10101c5f92f4506364
Pedro Dias <jpedrodias@gmail.com>
2025-12-15 11:05:09 +0000

    Learning C: Lesson 1

---
+++ LearningC/prog01.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Hello World\n");
+
+    // This will be an INTEGER 
+    float age = 25.25;
+
+    printf("My age is %f \n", age);
+    printf("This is %d bytes.\n", sizeof(age));
+
+    int a;
+    float b;
+    char s[10];
+    printf("a: %i %f %s \n", a, b, s);
+
+
+    return 0;
+}
+
